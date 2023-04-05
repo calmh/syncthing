9df8e605d8a98e90ba1c7f2d9b52b9426f251f9e
Jakob Borg <jakob@kastelo.net>
2023-04-05 10:31:40 +0200

    wip

--- lib/protocol/protocol.go
+++ lib/protocol/protocol.go
@@ -229,6 +229,47 @@ const (
 // Should not be modified in production code, just for testing.
 var CloseTimeout = 10 * time.Second
 
+// Underlying is the underlying connection we use for wire communication.
+// Mostly this is a ReadWriteCloser, i.e. a regular network socket of some
+// kind. This is referred to as the "primary" stream, and it is used for all
+// metadata messages.
+//
+// We also support "secondary" streams, which are used for sending data
+// requests and responses (only). Connection types that do not support this
+// should return ErrSecondaryStreamsUnsupported to indicate that all
+// communication should happen on the primary stream.
+//
+// When secondary streams are supported we will use one or more such streams
+// for data requests, for the purpose of increasing concurrency and
+// bandwidth.
+type Underlying interface {
+	io.ReadWriteCloser
+
+	// CreateSecondaryStream requests a new secondary stream. The returned
+	// ReadWriteCloser is the new stream, and the error is any error that
+	// occurred while creating the stream. An error in creating a secondary
+	// stream is not fatal -- the connection will continue to operate
+	// normally, using the primary stream instead. Creating a stream may
+	// have a certain overhead (e.g. TLS handshakes), so it is recommended
+	// to reuse streams for multiple requests. The stream should be closed
+	// once it is no longer required. Returning ErrSecondaryStreamsUnsupported
+	// from this method indicates that the connection does not support
+	// secondary streams.
+	CreateSecondaryStream() (io.ReadWriteCloser, error)
+
+	// AcceptSecondaryStream accepts a new secondary stream. The returned
+	// ReadWriteCloser is the new stream, and the error is any error that
+	// occurred while accepting the stream. An error in accepting a
+	// secondary stream is not fatal -- the connection will continue to
+	// operate normally, using the primary stream instead. If the underlying
+	// connection does not support secondary streams, this method should
+	// return ErrSecondaryStreamsUnsupported, in which case the accept call
+	// will not be retried for this connection.
+	AcceptSecondaryStream() (io.ReadWriter, error)
+}
+
+var ErrSecondaryStreamsUnsupported = errors.New("secondary streams not supported")
+
 func NewConnection(deviceID DeviceID, reader io.Reader, writer io.Writer, closer io.Closer, receiver Model, connInfo ConnectionInfo, compress Compression, passwords map[string]string, keyGen *KeyGenerator) Connection {
 	// Encryption / decryption is first (outermost) before conversion to
 	// native path formats.
