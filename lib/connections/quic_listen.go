5ef895d228a1dd4c96a0b38aacc93492374971bb
Jakob Borg <jakob@kastelo.net>
2023-04-13 13:12:04 +0200

    wip

--- lib/connections/quic_listen.go
+++ lib/connections/quic_listen.go
@@ -96,15 +96,15 @@ func (t *quicListener) serve(ctx context.Context) error {
 	}
 	defer func() { _ = udpConn.Close() }()
 
-	svc, conn := stun.New(t.cfg, t, udpConn)
-	defer conn.Close()
+	// svc, conn := stun.New(t.cfg, t, udpConn)
+	// defer conn.Close()
 
-	go svc.Serve(ctx)
+	// go svc.Serve(ctx)
 
-	t.registry.Register(t.uri.Scheme, conn)
-	defer t.registry.Unregister(t.uri.Scheme, conn)
+	t.registry.Register(t.uri.Scheme, udpConn)
+	defer t.registry.Unregister(t.uri.Scheme, udpConn)
 
-	listener, err := quic.Listen(conn, t.tlsCfg, quicConfig)
+	listener, err := quic.Listen(udpConn, t.tlsCfg, quicConfig)
 	if err != nil {
 		l.Infoln("Listen (BEP/quic):", err)
 		return err
