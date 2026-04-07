4cffa96d2b450184cede3d2e4a89b56283d03c6b
Jakob Borg <jakob@kastelo.net>
2026-04-07 15:31:52 +0200

    fix(protocol): limit size of incoming request messages
    Signed-off-by: Jakob Borg <jakob@kastelo.net>

--- lib/protocol/protocol_test.go
+++ lib/protocol/protocol_test.go
@@ -12,6 +12,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"io"
+	"strings"
 	"sync"
 	"testing"
 	"time"
@@ -538,6 +539,79 @@ func TestDispatcherToCloseDeadlock(t *testing.T) {
 	case <-c.dispatcherLoopStopped:
 	case <-time.After(time.Second):
 		t.Fatal("timed out before dispatcher loop terminated")
+	}
+}
+
+func TestRequestMaxSize(t *testing.T) {
+	m := newTestModel()
+	rw := testutil.NewBlockingRW()
+	c := getRawConnection(NewConnection(c0ID, rw, &testutil.NoopRW{}, testutil.NoopCloser{}, m, new(mockedConnectionInfo), CompressionAlways, testKeyGen))
+	c.Start()
+	defer closeAndWait(c, rw)
+
+	c.inbox <- &bep.ClusterConfig{}
+
+	// A request at exactly MaxRequestSize should be accepted.
+	c.inbox <- &bep.Request{
+		Id:   1,
+		Name: "valid",
+		Size: MaxRequestSize,
+	}
+
+	res := <-c.outbox
+	if msg, ok := res.msg.(*bep.Response); !ok || msg.Id != 1 {
+		t.Errorf("bad response %#v", msg)
+	}
+
+	// A request exceeding MaxRequestSize should cause the dispatcher to
+	// return with a protocol error.
+	c.inbox <- &bep.Request{
+		Id:   2,
+		Name: "too-large",
+		Size: MaxRequestSize + 1,
+	}
+
+	select {
+	case <-c.dispatcherLoopStopped:
+	case <-time.After(time.Second):
+		t.Fatal("timed out before dispatcher loop terminated")
+	}
+
+	err := m.closedError()
+	if err == nil {
+		t.Fatal("expected connection to be closed with an error")
+	}
+	if !strings.Contains(err.Error(), "protocol error") {
+		t.Errorf("expected a protocol error, got %v", err)
+	}
+}
+
+func TestRequestInvalidFilename(t *testing.T) {
+	m := newTestModel()
+	rw := testutil.NewBlockingRW()
+	c := getRawConnection(NewConnection(c0ID, rw, &testutil.NoopRW{}, testutil.NoopCloser{}, m, new(mockedConnectionInfo), CompressionAlways, testKeyGen))
+	c.Start()
+	defer closeAndWait(c, rw)
+
+	c.inbox <- &bep.ClusterConfig{}
+	c.inbox <- &bep.Request{
+		Id:   1,
+		Name: "../escape",
+		Size: 1024,
+	}
+
+	select {
+	case <-c.dispatcherLoopStopped:
+	case <-time.After(time.Second):
+		t.Fatal("timed out before dispatcher loop terminated")
+	}
+
+	err := m.closedError()
+	if err == nil {
+		t.Fatal("expected connection to be closed with an error")
+	}
+	if !strings.Contains(err.Error(), "protocol error") {
+		t.Errorf("expected a protocol error, got %v", err)
 	}
 }
 
